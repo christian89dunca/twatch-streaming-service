3e4b8606eb5c6cb5fb9432153eec1d98ff342bc6
Christian D. Dunca <christiand.dunca@Christians-MacBook-Pro.local>
2020-06-27 11:48:02 +0200

    streaming service added

--- streams/client/src/components/streams/StreamShow.jsx
+++ streams/client/src/components/streams/StreamShow.jsx
@@ -1,11 +1,40 @@
 import React from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 
 import { fetchStream } from "../../actions/index";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   renderStream() {
@@ -16,10 +45,12 @@ class StreamShow extends React.Component {
   }
 
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     return (
       <div>
-        <div className="video-placeholder-lg"></div>
+        <div className="video-placeholder-lg">
+          <video ref={this.videoRef} style={{ width: "80vw" }} controls />
+        </div>
         <h3>{this.renderStream().title}</h3>
         <h4>{this.renderStream().description}</h4>
       </div>
